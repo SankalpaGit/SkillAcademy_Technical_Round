1e9175d19917db0cfe0f7801eb33fbd518a84bf8
Sankalpa <joshisankalpa2@gmail.com>
2025-06-13 14:50:00 +0545

    authentication with Jwt complete backend

---
+++ authentication/__init__.py
@@ -0,0 +1,2 @@
+# app = 'authentication' authentication/__init__.py
+default_app_config = 'authentication.apps.AuthenticationConfig'
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# authentication/views.py
+from rest_framework import generics, permissions
+from django.contrib.auth.models import User
+from .models import Profile
+from .serializers import RegisterSerializer, ProfileSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
-# Create your views here.
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'register': reverse('register', request=request, format=format),
+        'profile': reverse('profile', request=request, format=format),
+        'login': reverse('token_obtain_pair', request=request, format=format),
+        'refresh_token': reverse('token_refresh', request=request, format=format),
+    })
+    
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterSerializer
+
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user.profile
